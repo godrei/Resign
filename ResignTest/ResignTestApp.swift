fe762da4179f12988c8e2f0200f29ade72d3ed9f
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-02-15 14:21:13 +0100

    Initial Commit

---
+++ ResignTest/ResignTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResignTestApp.swift
+//  ResignTest
+//
+//  Created by Krisztián Gödrei on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ResignTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
